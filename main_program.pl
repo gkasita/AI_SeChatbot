73e2d6ac1bf827686ab39cbb8178a2d0efa33975
Kasita Sansanthad <g.kasitasansanthad@gmail.com>
2023-11-22 17:33:29 +0700

    adding some pattern match (to be cont.)

--- main_program.pl
+++ main_program.pl
@@ -47,19 +47,32 @@ response_option('Admission') :-
 response_option('Syllabus') :-
     write('For our study-plan in KMITL, student can select to go through a track of Metaverse, Industial IoT, or Artificial Intelligence.'), nl, nl,
     write('Do you have a particular track you\'re interested in, or would you like us to provide syllabus for a specific semester, or perhaps all of them?'), nl,
+    repeat,
     read_line_to_string(user_input, UserInput),
-    contains_option_track(UserInput, Option) -> response_track(Option).
+    downcase_atom(UserInput, LowerUserInput),
+    contains_option_track(LowerUserInput, Option) -> response_track(Option);
+    handle_year_sem(LowerUserInput).
+    %in progress
 
+%for handle track
 response_track(Option) :-
     write('Here is the subject for '), write(Option), write(' track:'), nl,
     findall(Subject, subject(Subject, _, Option), Subjects),
-    print_subjects_list(Subjects).
+    print_subjects_list(Subjects),
+    write('To ask about other topics apart from syllabus, type back'), nl.
 
 print_subjects_list([]).
 print_subjects_list([Subject|Rest]) :-
     write('- '), write(Subject),nl,
     print_subjects_list(Rest).
 
+%for handle year and sem
+
+matches_pattern(Input, Year, Sem) :-
+    re_match("year\\s(\\d+)\\ssem\\s(\\d+)", Input, Captures),
+    Captures = [YearString, SemString],
+    atom_number(YearString, Year),
+    atom_number(SemString, Sem).
 
 
 
